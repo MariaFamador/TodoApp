f5c9206fffac73065c3b8925637a379762278792
Maria Famador <maria.famador87@gmail.com>
2014-12-31 02:49:06 +0800

    Configure Devise and permit extra parameters

---
+++ db/migrate/20141230133458_add_columns_to_devise_user.rb
@@ -0,0 +1,6 @@
+class AddColumnsToDeviseUser < ActiveRecord::Migration
+  def change
+    add_column :users, :fullname, :string
+    add_column :users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229180800) do
+ActiveRecord::Schema.define(version: 20141230133458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,10 @@ ActiveRecord::Schema.define(version: 20141229180800) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "fullname"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end

71ed0c54c37fae0fe0962e4b13a3af953bc07e48
Maria Famador <maria.famador87@gmail.com>
2015-01-04 21:52:29 +0800

    Add flash_messages from gist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  add_flash_types :success, :info, :warning, :danger
 
   protected
   def configure_permitted_parameters

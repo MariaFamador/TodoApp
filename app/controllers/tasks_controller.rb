25f9d8b7184dc8fb2789f23e6168b4be456c0c73
Maria Famador <maria.famador87@gmail.com>
2015-01-06 23:14:46 +0800

    Form and styling for new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def create
+    @task = Task.new(task_params)
+    if @task.save 
+      redirect_to "/", notice: "Task was successfully saved!"
+    else
+      render :new
+    end
+  end
+
+private
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :due_date,
+      :user_id
+      )
+  end
 end

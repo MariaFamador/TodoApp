622e7b0ff8d0844ba087c966571199dd90c30896
Maria Famador <maria.famador87@gmail.com>
2015-01-10 23:35:55 +0800

    Create partial for task container

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,11 +4,14 @@ class DashboardController < ApplicationController
   end
 
   def today
+    @tasks_today = Task.where(due_date: Date.today)
   end
 
   def tomorrow
+    @tasks_tomorrow = Task.where(due_date: Date.tomorrow)
   end
 
   def following_days
+    @tasks = Task.where.not(due_date: Date.today) && Task.where.not(due_date: Date.today) 
   end
 end
